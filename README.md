e2c238ff10d825319345468e4469c14e01506ff2
ViktorChizh <victor.chizh@mail.ru>
2024-06-06 13:14:41 +0300

    project finished

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# 1-todolist
+# todolist
+## viktorchizh.github.io/todolist/
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -36,7 +36,6 @@ export const App = ({ demo = false }: Props) => {
         <Container fixed style={{ margin: "0", padding: "0", width: "100%", maxWidth: "100%" }}>
           <Routes>
             <Route path={"/login"} element={<Login />} />
-            <Route path={"/1-todolist"} element={<Navigate to={"/todolists"} />} />
             <Route path={"/"} element={<Navigate to={"/todolists"} />} />
             <Route path={"/todolists"} element={<PageTodoLists demo={demo} />} />
             <Route path={"/error404"} element={<Error404 />} />
